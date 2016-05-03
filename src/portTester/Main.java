35f2add6832676263fb7f5ff14ba1e2f8487f25e
palko444 <stanka@ehs.sk>
2016-05-03 17:58:04 +0200

    wip

--- src/portTester/Main.java
+++ src/portTester/Main.java
@@ -1,15 +1,35 @@
 package portTester;
 
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.apache.commons.cli.CommandLine;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		String[] ports;
+		String[] servers;
+		CommandLine cl;
+		HashMap<String, HashMap<Integer, String>> results;
+
 		Main m = new Main();
 		ParseArgs pr = new ParseArgs(args);
-		pr.parseOptions();
-		System.out.println(m.IsPortOpen("localhost", 22));
+		pr.setOptions();
+
+		cl = pr.getParsedArgs();
+		ports = cl.getOptionValues("p");
+		servers = cl.getOptionValues("s");
+
+		results = m.runCheck(servers, ports);
+		
+		Iterator<String> it = results.keySet().iterator();
+		while(it.hasNext()){
+			String str = it.next();
+			System.out.println(str + results.get(str));
+		}
 	}
 
 	public boolean IsPortOpen(String host, int port) {
@@ -18,9 +38,25 @@ public class Main {
 			Socket ss = new Socket(host, port);
 			ss.close();
 			return true;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	public HashMap<String, HashMap<Integer, String>> runCheck(String[] servers, String[] ports) {
+		boolean status;
+		HashMap<String, HashMap<Integer, String>> results = new HashMap<String, HashMap<Integer, String>>();
+
+		for (String server : servers) {
+			HashMap<Integer, String> pp = new HashMap<Integer, String>();
+			results.put(server, pp);
+			for (String port : ports) {
+				int p = Integer.parseInt(port);
+				status = IsPortOpen(server, p);
+				String value = status ? "Open" : "Closed";
+				pp.put(p, value);
+			}
+		}
+		return results;
 	}
 }
--- src/portTester/ParseArgs.java
+++ src/portTester/ParseArgs.java
@@ -13,46 +13,41 @@ public class ParseArgs {
 	Options options = new Options();
 	CommandLineParser parser;
 	String[] args;
+	CommandLine cl;
 
 	public ParseArgs(String[] args) {
 		this.args = args;
-		setOptions();
 	}
 
 	public void setOptions() {
-		options.addOption(Option.builder("p")
-								.longOpt("port")
-								.hasArg(true)
-								.hasArgs()
-								.required()
-								.desc("Port to scan")
-								.build());
+		options.addOption(Option.builder("p").longOpt("port").hasArg(true).hasArgs().required(true).desc("Port to scan").build());
 		options.addOption("s", "system", true, "System to scan");
-		options.addOption("h", "help", false, "Print help");
-		options.addOption(Option.builder("f")
-								.longOpt("file")
-								.desc("File of hosts")
-								.build());
+		options.addOption(Option.builder("h").longOpt("help").required(false).desc("Print help").build());
+		options.addOption(Option.builder("f").longOpt("file").desc("File of hosts").build());
+		parseOptions();
 	}
 
 	public void printHelp() {
 		HelpFormatter hf = new HelpFormatter();
 		hf.printHelp("Port Tester", options);
-
+		System.exit(1);
 	}
 
 	public void parseOptions() {
 		parser = new DefaultParser();
 
 		try {
-			CommandLine cl = parser.parse(options, args);
-			System.out.println("Ports: " + cl.getOptionValue("p"));
-			System.out.println("System: " + cl.getOptionValue("s"));
-		}
-		catch (ParseException e) {
+			cl = parser.parse(options, args);
+			if (cl.hasOption("h")) {
+				printHelp();
+			}
+		} catch (ParseException e) {
 			printHelp();
-//			e.printStackTrace();
+			// e.printStackTrace();
 		}
+	}
 
+	public CommandLine getParsedArgs() {
+		return cl;
 	}
 }
